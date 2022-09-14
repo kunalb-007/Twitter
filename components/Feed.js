51a94214d64b90cbdc25b63a0044c8489407b50e
kunalb-007 <kunalbhandare422@gmail.com>
2022-09-14 23:44:18 +0530

    Created Post Section

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,31 @@
 import { SparklesIcon } from '@heroicons/react/solid'
 import React from 'react'
 import Input from './Input'
+import Post from './Post'
 
 export default function Feed() {
+
+    const posts = [
+        {
+            id: "1",
+            name: "ABC",
+            username: "abc123",
+            userImg: "/vercel.svg",
+            img: "/img1.jpg",
+            text: "Nice view!!",
+            timestamp: "2 hours ago"
+        },
+        {
+            id: "2",
+            name: "ABC",
+            username: "abc123",
+            userImg: "/vercel.svg",
+            img: "/img2.jpg",
+            text: "Wonderfull!!",
+            timestamp: "1 day ago"
+        }
+    ]
+
   return (
     <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[70px] flex-grow max-w-xl'>
         <div className='flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
@@ -12,6 +35,11 @@ export default function Feed() {
             </div>
         </div>
         <Input />
+
+        {posts.map((post) => (
+            <Post key={post.id} post={post} />
+        ))}
+
     </div>
   )
 }
