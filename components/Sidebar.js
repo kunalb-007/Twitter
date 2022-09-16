137193e94ce46422022b2d9746360c9a79349a04
kunalb-007 <kunalbhandare422@gmail.com>
2022-09-16 14:12:03 +0530

    Get the session and modify Sidebar and Input components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,8 +2,12 @@ import Image from "next/image";
 import SidebarMenu from "./SidebarMenu";
 import { HomeIcon } from "@heroicons/react/solid"
 import { BellIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, HashtagIcon, InboxIcon, UserIcon } from "@heroicons/react/outline";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Sidebar() {
+
+    const { data: session } = useSession();
+
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
 
@@ -16,27 +20,41 @@ export default function Sidebar() {
         <div className="mt-4 mb-2.5 xl:items-start">
             <SidebarMenu text="Home" Icon={HomeIcon} active />
             <SidebarMenu text="Explore" Icon={HashtagIcon} />
+
+            {session && (
+                <>
+                    <SidebarMenu text="Messages" Icon={InboxIcon} />
+                    <SidebarMenu text="Bookmarks" Icon={BookmarkIcon} />
+                    <SidebarMenu text="Lists" Icon={ClipboardIcon} />
+                    <SidebarMenu text="Profile" Icon={UserIcon} />
+                    <SidebarMenu text="More" Icon={DotsCircleHorizontalIcon} />
+                </>
+            )}
+
             <SidebarMenu text="Notifications" Icon={BellIcon} />
-            <SidebarMenu text="Messages" Icon={InboxIcon} />
-            <SidebarMenu text="Bookmarks" Icon={BookmarkIcon} />
-            <SidebarMenu text="Lists" Icon={ClipboardIcon} />
-            <SidebarMenu text="Profile" Icon={UserIcon} />
-            <SidebarMenu text="More" Icon={DotsCircleHorizontalIcon} />
+            
         </div>
 
 
         {/* Button */}
-        <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+        {session ? (
+            <>
+                <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
 
-        {/* Mini Profile */}
-        <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-            <img src="/vercel.svg" alt="" className="h-10 w-10 rounded-full xl:mr-2" />        {/* add ur image here */}
-            <div className="leading-5 hidden xl:inline">
-                <h4 className="font-bold">Kunal Bhandare</h4>
-                <p className="text-gray-500">@kunal_007</p>
-            </div>
-            <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-        </div>
+                {/* Mini Profile */}
+                <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+                    <img onClick={signOut} src={session.user.image} alt="" className="h-10 w-10 rounded-full xl:mr-2" />        {/* add ur image here */}
+                    <div className="leading-5 hidden xl:inline">
+                        <h4 className="font-bold">{session.user.name}</h4>
+                        <p className="text-gray-500">@{session.user.username}</p>
+                    </div>
+                    <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+                </div> 
+            </>
+        ) : (
+            <button onClick={signIn} className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Sign In</button>
+        )}
+        
 
     </div>
   )
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,7 +6,6 @@ import { getProviders, signIn } from "next-auth/react";
 export default function signin({ providers }) {
   return (
     <div className='flex justify-center mt-20 space-x-4'>
-        <img className='hidden object-cover md:w-44 md:h-80 rotate-6 md:inline-block' src='' alt='' />      {/* Add Image heres */}
         <div className=''>
             {Object.values(providers).map((provider) => (
                 <div className='flex flex-col items-center'>
