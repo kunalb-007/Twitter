16e5fd321e7d96e94c5586b2f2f68ffb07e686f6
kunalb-007 <kunalbhandare422@gmail.com>
2022-09-16 21:30:45 +0530

    Send comments to firestore and show number of comments

--- components/CommentsModal.js
+++ components/CommentsModal.js
@@ -4,9 +4,11 @@ import Modal from "react-modal";
 import { EmojiHappyIcon, PhotographIcon, XIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
-import { doc, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, doc, onSnapshot, serverTimestamp } from "firebase/firestore";
 import Moment from "react-moment";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 
 export default function CommentsModal() {
   const [open, setOpen] = useRecoilState(modalState);
@@ -14,14 +16,25 @@ export default function CommentsModal() {
   const [post, setPost] = useState({});
   const [input, setInput] = useState("");
   const { data: session } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     onSnapshot(doc(db, "posts", postId), (snapshot) => setPost(snapshot));
   }, [postId, db]);
 
 
-  function sendComment() {
+  async function sendComment() {
+    await addDoc(collection(db, "posts", postId, "comments"), {
+      comment: input,
+      name: session.user.name,
+      username: session.user.username,
+      userImg: session.user.image,
+      timestamp: serverTimestamp()
+    })
 
+    setOpen(false);
+    setInput("");
+    router.push(`posts/${postId}`);
   }
 
 
