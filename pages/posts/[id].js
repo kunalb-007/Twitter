11b43c9e3c963d251b9308ecb4adb16e2dd22fab
kunalb-007 <kunalbhandare422@gmail.com>
2022-09-16 22:47:13 +0530

    Get comments from firestore and display them in Comments section

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,19 +4,40 @@ import CommentsModal from "../../components/CommentsModal";
 import Sidebar from "../../components/Sidebar";
 import Widgets from "../../components/Widgets";
 import Post from "../../components/Post";
+import Comment from "../../components/Comment";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { db } from "../../firebase";
-import { doc, onSnapshot } from "firebase/firestore";
-
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
 export default function PostPage({ newsResults, randomUsers }) {
+  const router = useRouter();
+  const { id } = router.query;
+  const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
 
-    const router = useRouter();
-    const { id } = router.query;
-    const [post, setPost] = useState();
+  // get post data
+  useEffect(
+    () => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)),
+    [db, id]
+  );
 
-    useEffect(() => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)), [db, id])
+  // get comments
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => setComments(snapshot.docs)
+    );
+  }, [db, id]);
 
   return (
     <div>
@@ -34,16 +55,28 @@ export default function PostPage({ newsResults, randomUsers }) {
         <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[70px] flex-grow max-w-xl">
           <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
             <div className="hoverEffect" onClick={() => router.push("/")}>
-                <ArrowLeftIcon className="h-5" />
+              <ArrowLeftIcon className="h-5" />
             </div>
             <h2 className="text-lg sm:text-xl font-bold cursor-pointer">
               Tweet
             </h2>
           </div>
-        
-            <Post id={id} post={post} />
-        </div>
 
+          <Post id={id} post={post} />
+
+
+          {comments.length > 0 && (
+            <div className="">
+              {comments.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  id={comment.id}
+                  comment={comment.data()}
+                />
+              ))}
+            </div>
+          )}
+        </div>
 
 
         {/* Widgets */}
